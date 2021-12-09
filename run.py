aef9f9a77cb7971a6f9f5213a9c2b86e51b699c1
Anton Andersson <anton.andersson001@live.se>
2021-12-09 12:34:13 +0000

    Add validation function to check user input

--- run.py
+++ run.py
@@ -13,22 +13,29 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('strength_calculator')
 
-answers = SHEET.worksheet('answers')
+
+def validate_menu_input(data):
+    """
+    Checks if the user input is a number between 1 and 2.
+    """
+    try:
+        if int(data) != 1 and int(data) != 2:
+            raise ValueError(
+                f'A number between 1 or 2 is required. You entered {data}'
+            )
+    except ValueError as e:
+        print(f'Invalid input: {e}, please try again. \n')
 
 
-
-def main():
+def menu():
     print('Hello! Welcome to the strength calculator!\n')
     print('Please chose an option: ')
+
     print('Type 1 / 2\n')
     print('1. Check your average')
     print('2. View age average')
     menu_input = input()
-    
-
-    print(menu_input)
+    validate_menu_input(menu_input)
 
 
-main()
-
-
+menu()
